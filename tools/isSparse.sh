3bc7cab59c21d618b5fc13c83d674e1003511cfb
Iván Naranjo Ortega <ivanlink99@gmail.com>
2026-05-22 18:41:48 +0200

    tool: shorten header in isSparse.sh

--- tools/isSparse.sh
+++ tools/isSparse.sh
@@ -2,7 +2,7 @@
 ###############################################################################
 # isSparse.sh
 #
-# Checks how sparse a file is. Output is pipe-friendly (key value per line).
+# Checks how sparse a file is.
 ###############################################################################
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <file>" >&2
@@ -20,17 +20,17 @@ MIN_SPARSE_SIZE=512
 LOGICAL_SIZE=$(stat -c '%s' "$FILE")
 BLOCKS=$(stat -c '%b' "$FILE")
 BLOCK_SIZE=$(stat -c '%B' "$FILE")
-ALLOCATED_BYTES=$(( BLOCKS * BLOCK_SIZE ))
+ALLOCATED_BYTES=$((BLOCKS * BLOCK_SIZE))
 
 if [ "$LOGICAL_SIZE" -eq 0 ]; then
   echo "file=$FILE sparse=false logical_size=0 allocated=0 holes=0 sparseness=0.000%"
   exit 0
 fi
 
-HOLE_BYTES=$(( LOGICAL_SIZE - ALLOCATED_BYTES ))
+HOLE_BYTES=$((LOGICAL_SIZE - ALLOCATED_BYTES))
 SPARSENESS=$(awk "BEGIN { printf \"%.3f\", ($HOLE_BYTES / $LOGICAL_SIZE) * 100 }")
 
-if [ "$ALLOCATED_BYTES" -lt "$LOGICAL_SIZE" ]  && [ "$LOGICAL_SIZE" -gt "$MIN_SPARSE_SIZE" ]; then
+if [ "$ALLOCATED_BYTES" -lt "$LOGICAL_SIZE" ] && [ "$LOGICAL_SIZE" -gt "$MIN_SPARSE_SIZE" ]; then
   SPARSE=true
 else
   SPARSE=false
